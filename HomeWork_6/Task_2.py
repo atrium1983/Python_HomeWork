8182eed681e38fe6382115a833bb7ad227bc137d
Artem Cherepanov <artem.cherepanov530@gmail.com>
2023-09-19 10:57:03 +0200

    Homw work 6 is modified using teacher's comment

--- HomeWork_6/Task_2.py
+++ HomeWork_6/Task_2.py
@@ -11,11 +11,12 @@
 import random, os
 os.system('cls')
 
-min,max = int(input("Введите начальное значение = > ")), int(input("Введите конечное значение = > "))
+#min_elem,max_elem = int(input("Введите начальное значение = > ")), int(input("Введите конечное значение = > "))
+min_elem,max_elem = random.randint(1,5), random.randint(7,10) # совет преподавателя
 rand_list = [random.randint(-10,10) for _ in range (random.randint(10,15))]
 index_list = []
 for i in range(len(rand_list)):
-    if rand_list[i] >= min and rand_list[i] <= max:
+    if rand_list[i] >= min_elem and rand_list[i] <= max_elem:
         index_list.append(i)
 print(*rand_list)
 if len(index_list) == 0:
